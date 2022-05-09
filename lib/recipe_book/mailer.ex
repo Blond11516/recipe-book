2c1fc2099a674189f68fbb02f65369ce35a22b42
Étienne Lévesque <etienne.levesque16@gmail.com>
2022-05-08 21:30:26 -0400

    Add credo

--- lib/recipe_book/mailer.ex
+++ lib/recipe_book/mailer.ex
@@ -1,3 +1,5 @@
 defmodule RecipeBook.Mailer do
+  @moduledoc false
+
   use Swoosh.Mailer, otp_app: :recipe_book
 end
--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule RecipeBook.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", branch: "ms-declarative-api", override: true},
+      {:phoenix_live_view,
+       github: "phoenixframework/phoenix_live_view", branch: "ms-declarative-api", override: true},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
@@ -48,7 +49,8 @@ defmodule RecipeBook.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
