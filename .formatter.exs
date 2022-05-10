93c9875d707337aaf7283b3168b202ecba4b92d2
Étienne Lévesque <etienne.levesque16@gmail.com>
2022-05-09 21:24:15 -0400

    Configure HEEx formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
+  plugins: [Phoenix.LiveView.HTMLFormatter],
   import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["*.{hex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
 ]
