4d04b08fb99dad88159d3d40a27c67d3e254e44b
Étienne Lévesque <etienne.levesque16@gmail.com>
2022-08-31 03:10:18 +0000

    chore(deps): update dependency ecto_sqlite3 to == 0.8.1

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule RecipeBook.MixProject do
       {:phoenix, "== 1.6.11"},
       {:phoenix_ecto, "== 4.4.0"},
       {:ecto_sql, "== 3.8.3"},
-      {:ecto_sqlite3, "== 0.8.0"},
+      {:ecto_sqlite3, "== 0.8.1"},
       {:postgrex, "== 0.16.4"},
       {:phoenix_html, "== 3.2.0"},
       {:phoenix_live_reload, "== 1.3.3", only: :dev},
