a9881ed8d71a6e7977bef63fdf719cdea65c83c3
Étienne Lévesque <contact@etiennel.dev>
2022-09-04 21:43:59 +0000

    chore(deps): update dependency typescript to v4.8.2

--- e2e-tests/.pnp.cjs
+++ e2e-tests/.pnp.cjs
@@ -35,7 +35,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["@testing-library/cypress", "virtual:a62d31b418fd61b9efda6050256d5bec21dc9f4e02f9926150d6ae257d13727c1f8f7508b60cb6a896d7286369442a53b7d628bf569405f7d798dd49ab693dad#npm:8.0.3"],\
             ["cypress", "npm:10.5.0"],\
-            ["typescript", "patch:typescript@npm%3A4.7.4#~builtin<compat/typescript>::version=4.7.4&hash=f456af"]\
+            ["typescript", "patch:typescript@npm%3A4.8.2#~builtin<compat/typescript>::version=4.8.2&hash=f456af"]\
           ],\
           "linkType": "SOFT"\
         }]\
@@ -806,7 +806,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["e2e-tests", "workspace:."],\
             ["@testing-library/cypress", "virtual:a62d31b418fd61b9efda6050256d5bec21dc9f4e02f9926150d6ae257d13727c1f8f7508b60cb6a896d7286369442a53b7d628bf569405f7d798dd49ab693dad#npm:8.0.3"],\
             ["cypress", "npm:10.5.0"],\
-            ["typescript", "patch:typescript@npm%3A4.7.4#~builtin<compat/typescript>::version=4.7.4&hash=f456af"]\
+            ["typescript", "patch:typescript@npm%3A4.8.2#~builtin<compat/typescript>::version=4.8.2&hash=f456af"]\
           ],\
           "linkType": "SOFT"\
         }]\
@@ -1922,10 +1922,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["typescript", [\
-        ["patch:typescript@npm%3A4.7.4#~builtin<compat/typescript>::version=4.7.4&hash=f456af", {\
-          "packageLocation": "./.yarn/cache/typescript-patch-fc259c5d56-9096d8f6c1.zip/node_modules/typescript/",\
+        ["patch:typescript@npm%3A4.8.2#~builtin<compat/typescript>::version=4.8.2&hash=f456af", {\
+          "packageLocation": "./.yarn/cache/typescript-patch-a60f043126-6f49363af8.zip/node_modules/typescript/",\
           "packageDependencies": [\
-            ["typescript", "patch:typescript@npm%3A4.7.4#~builtin<compat/typescript>::version=4.7.4&hash=f456af"]\
+            ["typescript", "patch:typescript@npm%3A4.8.2#~builtin<compat/typescript>::version=4.8.2&hash=f456af"]\
           ],\
           "linkType": "HARD"\
         }]\
