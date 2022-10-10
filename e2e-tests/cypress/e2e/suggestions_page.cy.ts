649cc05d9870ddad13c4f99c6097e44c337aeeab
Étienne Lévesque <contact@etiennel.dev>
2022-10-10 21:58:08 +0000

    test: Setup playwright and migrate test

--- e2e-tests/cypress/e2e/suggestions_page.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('Suggestions page', () => {
-	it('should display three recipes', () => {
-		cy.visit('http://localhost:4000')
-		cy.findByRole('list', { name: /Suggestions de recettes/i }).within(($list) => {
-			cy.findAllByRole('listitem').should('have.length', 3)
-		})
-	})
-})
