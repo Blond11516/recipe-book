74ba12fda5b9fe8a4cc61a16574ebe0fce8d6c93
Étienne Lévesque <contact@etiennel.dev>
2022-11-13 15:53:17 +0000

    chore(deps): Update esbuild

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :recipe_book, RecipeBookWeb.Endpoint,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.29",
+  version: "0.15.13",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --sourcemap),
