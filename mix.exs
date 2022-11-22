aa493b414ee72401fcae8f17e71bf2ef81ec82e9
Étienne Lévesque <contact@etiennel.dev>
2022-11-22 03:07:24 +0000

    chore(deps): update dependency opentelemetry_ecto to == 1.1.0

--- mix.exs
+++ mix.exs
@@ -87,7 +87,7 @@ defmodule RecipeBook.MixProject do
        runtime: Mix.env() == :prod or System.get_env("DEBUG_OPENTELEMETRY") == "true"},
       {:opentelemetry_api, "== 1.1.1"},
       {:opentelemetry_exporter, "== 1.2.2"},
-      {:opentelemetry_ecto, "== 1.0.0"},
+      {:opentelemetry_ecto, "== 1.1.0"},
       {:opentelemetry_liveview, "== 1.0.0-rc.4"},
       {:opentelemetry_phoenix, "== 1.0.0"},
       {:faker, "== 0.17.0", only: [:dev, :test]},
