abf1816e7aa79cf8647e7b5724ba48c2c3f1056b
Étienne Lévesque <contact@etiennel.dev>
2022-12-21 18:22:16 -0500

    update esbuild to 0.16.10

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :recipe_book, RecipeBookWeb.Endpoint,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.15.13",
+  version: "0.16.10",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --sourcemap),
