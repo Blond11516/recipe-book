7e7470a207f66e47faf477df95716ba592cdc859
Étienne Lévesque <contact@etiennel.dev>
2023-01-01 16:27:41 -0500

    build: Migrate with deploy command instead of on app start

--- lib/recipe_book_application.ex
+++ lib/recipe_book_application.ex
@@ -8,7 +8,6 @@ defmodule RecipeBookApplication do
 
   @impl true
   def start(_type, _args) do
-    migrate_if_prod()
     setup_opentelemetry()
 
     children = [
@@ -42,13 +41,5 @@ defmodule RecipeBookApplication do
     OpentelemetryPhoenix.setup()
     OpentelemetryLiveView.setup()
     OpentelemetryEcto.setup([:recipe_book, :repo])
-  end
-
-  if Mix.env() == :prod do
-    defp migrate_if_prod do
-      RecipeBookRelease.migrate()
-    end
-  else
-    defp migrate_if_prod, do: :ok
   end
 end
