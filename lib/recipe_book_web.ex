20e3ae019929b91a7cc472748ee698e692324325
Étienne Lévesque <contact@etiennel.dev>
2023-03-10 21:58:37 -0500

    refactor: Migrate to verified routes (#63)

--- lib/recipe_book_web.ex
+++ lib/recipe_book_web.ex
@@ -1,3 +1,5 @@
 defmodule RecipeBookWeb do
   use Boundary, deps: [RecipeBook, Phoenix, Ecto.Changeset], exports: [Endpoint]
+
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 end
